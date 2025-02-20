61f18f9b704f9b882d3641bb4f398a3d61c83938
Jhuliño A. Oroya Barreto <jaob.4.95@gmail.com>
2025-02-20 07:25:20 -0500

    Chore: Update base path for deployment
    vite.config.js: Updated the base path from '/equipo-s21-18-m-webapp/' to '/fitlover-webapp/' to reflect the new repository name and ensure proper deployment.

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/equipo-s21-18-m-webapp/',
+  base: '/fitlover-webapp/',
 });
